ee30d2adc4f65257e0f2fb9a9bd3cac40d957b54
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2024-11-07 11:36:26 +0100

    pull imported files from properties

--- src/main/java/org/identifiers/cloud/ws/sparql/configurations/RepositoryConfiguration.java
+++ src/main/java/org/identifiers/cloud/ws/sparql/configurations/RepositoryConfiguration.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.ws.sparql.configurations;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.identifiers.cloud.ws.sparql.services.SameAsResolver;
 import org.identifiers.cloud.ws.sparql.data.sail.IdorgStore;
@@ -14,30 +15,32 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class RepositoryConfiguration {
-	@Bean
-	public Repository getRepository(SameAsResolver sameAsResolver,
-                                    @Value("${org.identifiers.cloud.ws.sparql.idotdatafolder}")
-                                    File dataDir) throws IOException{
+    @Bean
+    public Repository getRepository(SameAsResolver sameAsResolver,
+                                    @Value("${org.identifiers.cloud.ws.sparql.imports}")
+                                    URL[] imports) throws IOException {
         var idotStore = new MemoryStore();
-		SailRepository updatableRepository = new SailRepository(idotStore);
-		updatableRepository.init();
-		try (var conn = updatableRepository.getConnection()) {
-			var idotFile = new File(dataDir, "idorg-ontology.ttl");
-			conn.add(idotFile, null, RDFFormat.TURTLE);
+        SailRepository updatableRepository = new SailRepository(idotStore);
+        updatableRepository.init();
+        try (var conn = updatableRepository.getConnection()) {
+            for (URL importUrl : imports) {
+                var format = Rio.getParserFormatForFileName(importUrl.toString())
+                        .orElse(RDFFormat.RDFXML);
+                conn.add(importUrl, null, format);
+            }
+        }
 
-			var triplesFile = new File(dataDir, "idorg-ontology-materialized.ttl");
-			conn.add(triplesFile, null, RDFFormat.TURTLE);
-		}
+        IdorgStore idorgStore = new IdorgStore(sameAsResolver);
+        idorgStore.setBaseSail(idotStore);
+        SailRepository sailRepository = new SailRepository(idorgStore);
+        sailRepository.init();
 
-		IdorgStore idorgStore = new IdorgStore(sameAsResolver);
-		idorgStore.setBaseSail(idotStore);
-		SailRepository sailRepository = new SailRepository(idorgStore);
-		sailRepository.init();
-
-		return sailRepository;
-	}
+        return sailRepository;
+    }
 }
