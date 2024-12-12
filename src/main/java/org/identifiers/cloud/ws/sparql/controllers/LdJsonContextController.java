b217e20bd9b71e9593e767b2e69ffb068f6e3cc6
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2024-12-12 10:37:05 -0300

    Enforce LD JSON response type

--- src/main/java/org/identifiers/cloud/ws/sparql/controllers/LdJsonContextController.java
+++ src/main/java/org/identifiers/cloud/ws/sparql/controllers/LdJsonContextController.java
@@ -2,6 +2,7 @@ package org.identifiers.cloud.ws.sparql.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.identifiers.cloud.ws.sparql.data.LdJsonContextService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +17,17 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 @CrossOrigin(originPatterns = "*", maxAge = 3600)
 public class LdJsonContextController {
     private final LdJsonContextService ldJsonContextService;
+    private final MediaType ldJsonMediaType = MediaType.parseMediaType("application/ld+json");
 
-    @GetMapping(value = "/context", produces = {APPLICATION_JSON_VALUE, "application/ld+json"})
+    @GetMapping(value = "/context")
     ResponseEntity<Map<String, Map<String, String>>> getContexts() {
         var jsonLdContexts = ldJsonContextService.getJsonLdContexts();
         if (jsonLdContexts.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
-            return ResponseEntity.ok().body(jsonLdContexts);
+            return ResponseEntity.ok()
+                    .contentType(ldJsonMediaType)
+                    .body(jsonLdContexts);
         }
     }
 }
